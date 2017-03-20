3f6c0400f6ccbc945165cc32133f138bc6edc629
jinglf000 <jinglf000@gmail.com>
2017-03-20 18:01:42 +0800

    双向数据绑定

---
+++ build/index.js
@@ -0,0 +1,26 @@
+// 引用
+let express = require("express"),
+    proxy   = require("express-http-proxy"),
+    bodyParser = require("body-parser"),
+    path = require("path");
+
+// 配置项
+let app = express(),
+    port = 9099,
+    proxyUrl = "http://60.205.13.50";
+
+// 代理API
+let apiProxy = proxy(proxyUrl,{
+    forwardPath : function(req,res){
+        return req._parsedUrl.path;
+    }
+});
+app.use("/proxy/*",apiProxy);
+
+app.use(express.static(path.join(__dirname.replace("\\build",""))));
+
+console.log(path.join(__dirname.replace("/build","")));
+
+app.listen(port);
+
+console.log("serve is runing at :" + port);
